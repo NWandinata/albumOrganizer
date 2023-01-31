26d419ffc3bc3dbe2e8abe5d429464e1527c9d0b
Nicholas Andrew Wandinata <wrx481@hydra0.eecs.utk.edu>
2023-01-31 18:43:17 -0500

    Setup

--- proj1/lib_info.cpp
+++ proj1/lib_info.cpp
@@ -1,19 +1,33 @@
 /*Makenzie Johnson and Nicholas Wandinata*/
 #include <iostream>
 #include <fstream>
-#include <strings>
+#include <string>
+#include <algorithm>
+#include <map>
 #include "lib_info.h"
+// If compilation error, try including iterator
 
 using namespace std;
 
 int main(int argc, char **argv) {
 	string musicInfo;
+	// map<string, Artist> map; - This may or may not be necessary
+	Artist entryAr;
+	Album entryAl;
+	Song entryS;
 
 	ifstream file;
 	file.open(argv[1]);
 	while(!file.eof()) {
 		getline(file, musicInfo);
 		replace(musicInfo.begin(), musicInfo.end(), '_', ' ');
+		// Insert into struct here
+
+	}
+
+	for(map<string,Artist>::iterator it= artists.begin(); it != artists.end(); ++it) {
+   		cout << it->first << ": " << it->second.nsongs << ", ";
+   		printTime(it->second.time);
 	}
 
 	file.close();
