fbc45f3b7b18c0108318c1fea12e43a3affa0f19
Makenzie Ryan Johnson <mjohn326@tesla14.eecs.utk.edu>
2023-01-31 17:18:36 -0500

    Added the structs

--- proj1/lib_info.h
+++ proj1/lib_info.h
@@ -1,2 +1,22 @@
 //Header file
 //Makenzie Johnson and Nicholas Wandinata
+
+struct Song {
+    string title;
+    int time;  // could also be a string
+};
+
+struct Album {
+    map <int, Song > songs;
+    string name;
+    int time;
+    int nsongs;  // optional variable but makes it easier
+};
+
+struct Artist {
+    map <string, Album > albums;
+    string name;
+    int time;
+    int nsongs;
+};
+
